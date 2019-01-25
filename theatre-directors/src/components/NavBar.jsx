b30472049580e1b6632c0f748187b6bcc31ba2e1
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2019-01-26 01:14:28 +0300

    #12/feat: add navigation

---
+++ theatre-directors/src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function NavBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Title placeholder
+          </Typography>
+          <Button color="inherit">En</Button>
+          <Button color="inherit">Ru</Button>
+          <Button color="inherit">By</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+NavBar.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+};
+
+export default withStyles(styles)(NavBar);
---
+++ theatre-directors/src/pages/Developers.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Developers() {
+  return (
+    <div className="App">
+        Developers
+    </div>);
+}
+
+export default Developers;
