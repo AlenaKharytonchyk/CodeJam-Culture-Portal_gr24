93e7580efdc7a62b6f43b3491db7e16bb6e4cfb4
hushcha <375291601334@mail.ru>
2019-01-26 17:33:39 +0300

    feat: developer tab

---
+++ theatre-directors/src/components/DevelopersCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const fotos = require.context('../assets/images/developers', false, /\.jpg$/);
+const fotosObj = fotos.keys()
+  .reduce((imgs, key) => {
+    imgs[key] = fotos(key);
+    return imgs;
+  }, {});
+
+function DeveloperCard(devInfo) {
+  const devHref = `https://github.com/${devInfo.github}`;
+  const fotoSrc = `./${devInfo.img}`;
+  const SIZE = 190;
+  return (
+    <div className="developer-card">
+      <img src={fotosObj[fotoSrc]} alt="foto" width={SIZE} height={SIZE} />
+      <h2>{devInfo.name}</h2>
+      <a href={devHref}>
+        <FontAwesomeIcon
+          icon="envelope"
+          color="6600bf"
+          size="lg"
+        />
+        github: {devInfo.github}
+      </a>
+    </div>
+  );
+}
+
+export default DeveloperCard;
