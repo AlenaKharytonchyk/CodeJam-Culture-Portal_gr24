2ca68c113fed3031cad0c171dd71d12d284975fa
hushcha <375291601334@mail.ru>
2019-01-26 22:34:46 +0300

    feat: add github sign

--- theatre-directors/src/App.jsx
+++ theatre-directors/src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import CssBaseLine from '@material-ui/core/CssBaseline';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import NavBar from './components/NavBar';
 import PageContainer from './components/PageContainer';
 import Home from './pages/Home';
 import Directors from './pages/Directors';
 import Developers from './pages/Developers';
-
-library.add(faEnvelope);
 
 function App() {
   return (
