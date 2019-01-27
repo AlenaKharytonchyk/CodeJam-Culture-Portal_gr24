a65aa620974c41752e8956a34e150db7012f9830
Victoria Likhuta <v1ktor1a09051994@gmail.com>
2019-01-27 13:08:38 +0300

    #6/feat: divide DirectorDetails info into relevant components

---
+++ theatre-directors/src/components/DirectorDetailsComponents/Compositions.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Compositions(props) {
+  const { result } = props;
+  const listItems = result.compositions.map(item => <li>{item}</li>);
+  return (
+    <div className="compositions">
+      <h3>Произведения автора</h3>
+      <ul>{ listItems }</ul>
+    </div>
+  );
+}
+
+Compositions.propTypes = {
+  result: PropTypes.shape({}).isRequired,
+};
---
+++ theatre-directors/src/components/DirectorDetailsComponents/DirectorDetails.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import GeneralInfo from './GeneralInfo';
+import TimeLine from './TimeLine';
+import Map from './Map';
+import Compositions from './Compositions';
+import Gallery from './Gallery';
+import Youtube from './Youtube';
+
+export default function DirectorDetails(props) {
+  const { result } = props;
+  return (
+    <section className="full-info">
+      <GeneralInfo />
+      <TimeLine />
+      <Compositions />
+      <Gallery />
+      <Youtube />
+      <Map coordinates={result.coordinates} />
+    </section>
+  );
+}
+
+DirectorDetails.propTypes = {
+  result: PropTypes.shape({}).isRequired,
+};
