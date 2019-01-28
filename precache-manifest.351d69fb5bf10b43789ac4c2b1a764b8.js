464c4a13c14038b2199804883a978a23064a0fa1
AlenaKharytonchyk <alena.kharytonchyk@outlook.com>
2019-01-28 21:45:55 +0300

    Updates

---
+++ precache-manifest.351d69fb5bf10b43789ac4c2b1a764b8.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8b7698af46af482715258f3288186ce",
+    "url": "/static/media/4.f8b7698a.jpg"
+  },
+  {
+    "revision": "e3b8aa8971f944d25f53",
+    "url": "/static/css/main.5df43c6b.chunk.css"
+  },
+  {
+    "revision": "85564dc40b6f98d5a46500f3aa467a5d",
+    "url": "/static/media/VictoriaTheBrave.85564dc4.jpg"
+  },
+  {
+    "revision": "6c44fbbccbbfd5088c0e",
+    "url": "/static/js/1.6c44fbbc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2727bd746f667a7ad719f2c5b93236bb",
+    "url": "/static/media/2Bujnicki.2727bd74.jpg"
+  },
+  {
+    "revision": "1d134209fa3792210f1de9d7ad3dc2e1",
+    "url": "/static/media/3Dervoed.1d134209.jpg"
+  },
+  {
+    "revision": "c9ad6749cec775456fb7dcf7cebe4813",
+    "url": "/static/media/1.c9ad6749.jpg"
+  },
+  {
+    "revision": "bc63fb8ac695475b3762cf8ac3a2a214",
+    "url": "/static/media/2.bc63fb8a.jpg"
+  },
+  {
+    "revision": "df8696952b51890010a905f665385a10",
+    "url": "/static/media/3.df869695.jpg"
+  },
+  {
+    "revision": "4673fa882c4033be00e86c9719b9278b",
+    "url": "/static/media/5.4673fa88.jpg"
+  },
+  {
+    "revision": "d7179202d868d094e39ff5bf6e8fe95d",
+    "url": "/static/media/1.d7179202.jpg"
+  },
+  {
+    "revision": "5d27c3996035f833cb8db1e12b098f7a",
+    "url": "/static/media/2.5d27c399.jpg"
+  },
+  {
+    "revision": "7d42a8fdf6e08640559c8c8185d9acf0",
+    "url": "/static/media/3.7d42a8fd.jpg"
+  },
+  {
+    "revision": "e3b8aa8971f944d25f53",
+    "url": "/static/js/main.e3b8aa89.chunk.js"
+  },
+  {
+    "revision": "f0132bf002e2e7bea07a1e4a7a227d44",
+    "url": "/static/media/5.f0132bf0.jpg"
+  },
+  {
+    "revision": "1d134209fa3792210f1de9d7ad3dc2e1",
+    "url": "/static/media/1.1d134209.jpg"
+  },
+  {
+    "revision": "2c72fed5e5aafc30893c80060438a3fd",
+    "url": "/static/media/2.2c72fed5.jpg"
+  },
+  {
+    "revision": "7dc35210c1b29cb77c52f24ea025bcfa",
+    "url": "/static/media/4.7dc35210.jpg"
+  },
+  {
+    "revision": "3df521815db20a68c05992cd4f3510ea",
+    "url": "/static/media/5.3df52181.jpg"
+  },
+  {
+    "revision": "8b863aeef580fd96142ae4882f0e9a73",
+    "url": "/static/media/2.8b863aee.jpg"
+  },
+  {
+    "revision": "2ddf940408ba89e0e640126432d6e479",
+    "url": "/static/media/3.2ddf9404.jpg"
+  },
+  {
+    "revision": "73b2fac4fdc13e144468806d8fc915ec",
+    "url": "/static/media/5.73b2fac4.jpg"
+  },
+  {
+    "revision": "7f368c6e665c6d6ca381976c8ed458b6",
+    "url": "/static/media/AnastasiyaHushcha.7f368c6e.jpg"
+  },
+  {
+    "revision": "1b0321ebd499fc571dcbb4e10832d8a9",
+    "url": "/static/media/JulianKov.1b0321eb.jpg"
+  },
+  {
+    "revision": "1e1c6dcea4f8e0099b257907a03f53f6",
+    "url": "/static/media/AlenaKharytonchyk.1e1c6dce.jpg"
+  },
+  {
+    "revision": "6c44fbbccbbfd5088c0e",
+    "url": "/static/css/1.b6f3be20.chunk.css"
+  },
+  {
+    "revision": "30f494dac3c9206011855802ba367307",
+    "url": "/index.html"
+  }
+];
